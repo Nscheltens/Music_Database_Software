236f9daf20bf707e5df5cda5fdf79d5799b9be2b
Nscheltens <36109516+Nscheltens@users.noreply.github.com>
2018-12-18 12:08:17 -0600

    added scan for albums

--- Music_Databases_Control/src/main/java/com/mycompany/music_databases_control/Data_Scanner.java
+++ Music_Databases_Control/src/main/java/com/mycompany/music_databases_control/Data_Scanner.java
@@ -35,7 +35,7 @@ public class Data_Scanner {
         if(cutList.length == 0){
             System.out.println("No correctly formated files found.");
         }else{
-            handelfiles(cutList,"Song");
+            handelFiles(cutList,"Song");
         }
     }
     private String[][] processSongNames(String[] list, String direct){
@@ -49,7 +49,10 @@ public class Data_Scanner {
                 //System.out.println("going to cut number off "+list1);
                 String[] NumRemoved = list1.split("[0-9]{2}+ ");
                 //System.out.println("going to cut extention off "+NumRemoved[1]);
+                
+                //fix to look for flac and mp3
                 String[] SongName = NumRemoved[1].split(".mp3");
+                
                 //System.out.println("adding "+SongName[0]);
                 cuttingList[listMover][0] = SongName[0];
                 File path = new File(direct, list1);
@@ -72,12 +75,46 @@ public class Data_Scanner {
         File directory = new File(direct);
         String[] directoryContents = directory.list();
         System.out.println("Listing files in directory");
-        for(String fileName: directoryContents){
-            File temp = new File(directory, fileName);
-            
-            System.out.println(fileName);
+        String[][] cutList = processAlbumNames(directoryContents, direct);
+        if(cutList.length == 0){
+            System.out.println("No correctly formated files found.");
+        }else{
+            handelFiles(cutList,"Album");
         }
     }
+    private String[][] processAlbumNames(String[] list, String direct){
+        int listMover = 0;
+        int numSongs = list.length;
+        String[][] cuttingList = new String[list.length][6];
+        Pattern p = Pattern.compile("\\[.++");
+        for (String list1 : list) {
+            Matcher m = p.matcher(list1);
+            if (m.matches()) {
+                String[] spaceSplit = list1.split(" ");
+                cuttingList[listMover][2] = spaceSplit[0];
+                cuttingList[listMover][3] = spaceSplit[1];
+                
+                String[] nameSplit1 = list1.split("-\\s");
+                String[] nameSplit2 = nameSplit1[1].split("\\s\\{");
+                cuttingList[listMover][0] = nameSplit2[0];
+                
+                String[] ripSplit = list1.split("[\\{\\}]");
+                cuttingList[listMover][4] = ripSplit[1];
+                
+                String[] fileSplit = list1.split("[\\(\\)]");
+                cuttingList[listMover][5] = fileSplit[1];
+                //System.out.println("date = "+cuttingList[listMover][2]+" artist = "+cuttingList[listMover][3]+" name = "+cuttingList[listMover][0]+" Rip = "+cuttingList[listMover][4]+" filetype = "+cuttingList[listMover][5]);
+                
+                File path = new File(direct, list1);
+                cuttingList[listMover][1] = path.getAbsolutePath();
+                listMover++;
+            } else{
+                numSongs--;
+            }
+        }
+        return Arrays.copyOf(cuttingList, numSongs);
+    }
+    
     public void ScanForArtists(String direct){
         System.out.println(direct);
         File directory = new File(direct);
@@ -96,7 +133,7 @@ public class Data_Scanner {
     private void ArtistNameHnadeler(String SongName){
         
     }
-    private void handelfiles(String[][] items, String type){
+    private void handelFiles(String[][] items, String type){
         Scanner in = new Scanner(System.in);
         System.out.println("Scanner found these "+type+"s");
         System.out.println("-------------------------------------------------");
